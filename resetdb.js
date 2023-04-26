66f0c873eebd4c747643319ffbf70f3bc6ea7908
Matt <msmi200@aucklanduni.ac.nz>
2023-04-26 21:27:40 +1200

    Nurse conflict testing working, appointments displayed in table at index.html

---
+++ resetdb.js
@@ -0,0 +1,37 @@
+const sqlite3 = require('sqlite3').verbose();
+const fs = require('fs');
+
+function resetDatabase(db) {
+  db.serialize(() => {
+    db.run("DROP TABLE IF EXISTS appointments", (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log("Dropped appointments table");
+      }
+    });
+
+    const schema = fs.readFileSync('schema.sql').toString();
+    db.exec(schema, (err) => {
+      if (err) {
+        console.error(err.message);
+      } else {
+        console.log('Tables created.');
+      }
+    });
+  });
+}
+
+function initializeDatabase() {
+  const db = new sqlite3.Database('database.db', (err) => {
+    if (err) {
+      console.error(err.message);
+    } else {
+      console.log('Connected to the database.');
+      resetDatabase(db);
+      db.close();
+    }
+  });
+}
+
+initializeDatabase();
