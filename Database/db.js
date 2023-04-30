9b876be3e798aac1aec962d66f7cfe96a27788f6
Matt <msmi200@aucklanduni.ac.nz>
2023-04-30 19:36:58 +1200

    improve app.js and db.js and interactions

--- Database/db.js
+++ Database/db.js
@@ -1,36 +1,37 @@
 const sqlite3 = require('sqlite3').verbose();
 const fs = require('fs');
-let db;
 
-function initializeDatabase(callback) {
-  db = new sqlite3.Database('database.db', (err) => {
-    if (err) {
-      console.error(err.message);
-    } else {
-      console.log('Connected to the database.');
-      const schema = fs.readFileSync('./Database/schema.sql').toString();
-      db.exec(schema, (err) => {
-        if (err) {
-          console.error(err.message);
-        } else {
-          console.log('Tables created.');
-          callback(db);
-        }
-      });
-    }
+function initializeDatabase() {
+  return new Promise((resolve, reject) => {
+    const db = new sqlite3.Database('database.db', (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        console.log('Connected to the database.');
+        const schema = fs.readFileSync('./Database/schema.sql').toString();
+        db.exec(schema, (err) => {
+          if (err) {
+            reject(err);
+          } else {
+            console.log('Tables created.');
+            resolve(db);
+          }
+        });
+      }
+    });
   });
 }
 
 // Wrapper function for db.get() that returns a Promise
 function getPromise(query, params, db) {
   return new Promise((resolve, reject) => {
-      db.get(query, params, (err, row) => {
-          if (err) {
-              reject(err);
-          } else {
-              resolve(row);
-          }
-      });
+    db.get(query, params, (err, row) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(row);
+      }
+    });
   });
 }
 
--- app.js
+++ app.js
@@ -1,30 +1,38 @@
 const express = require('express');
-const { initializeDatabase, getPromise } = require('./Database/db.js');
 const ResourceBaseAvailabilityChecker = require('./Helpers/resourceBaseAvailabilityCheckerService.js');
 const appointments = require('./routes/appointments.js');
-
+const db = require('./Database/db.js');
 const fs = require('fs');
-const app = express();
-const port = 3000;
 
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(express.json());
 
 // Read in nurse trialAvailability from JSON file
-const nurseAvailability = JSON.parse(fs.readFileSync('./Config/nurse_availability.json'));
-const researcherAvailability = JSON.parse(fs.readFileSync('./Config/researcher_availability.json'));
-const trialAvailability = JSON.parse(fs.readFileSync('./Config/trial_availability.json'));
-const psychologistOneAvailability = JSON.parse(fs.readFileSync('./Config/psychologist_1_availability.json'));
-const psychologistTwoAvailability = JSON.parse(fs.readFileSync('./Config/psychologist_2_availability.json'))
+const nurseAvailability = JSON.parse(
+  fs.readFileSync('./Config/nurse_availability.json')
+);
+const researcherAvailability = JSON.parse(
+  fs.readFileSync('./Config/researcher_availability.json')
+);
+const trialAvailability = JSON.parse(
+  fs.readFileSync('./Config/trial_availability.json')
+);
+const psychologistOneAvailability = JSON.parse(
+  fs.readFileSync('./Config/psychologist_1_availability.json')
+);
+const psychologistTwoAvailability = JSON.parse(
+  fs.readFileSync('./Config/psychologist_2_availability.json')
+);
 
+db.initializeDatabase()
+  .then((database) => {
+    const checker = new ResourceBaseAvailabilityChecker(database, db.getPromise);
+    console.log('Database initialized');
 
-initializeDatabase((database) => {
-    db = database;
-    const checker = new ResourceBaseAvailabilityChecker(db, getPromise);
-    console.log("Database initialized")
-
-    app.locals.db = db;
+    app.locals.db = database;
     app.locals.checker = checker;
     app.locals.nurseAvailability = nurseAvailability;
     app.locals.researcherAvailability = researcherAvailability;
@@ -32,12 +40,21 @@ initializeDatabase((database) => {
 
     app.use('/appointments', appointments);
 
-
-    //Get all availability for screening appointments
+    // Get all availability for screening appointments
     TODO:
+
+    // Error handling middleware
+    app.use((err, req, res, next) => {
+      console.error(err);
+      res.status(500).send('Something went wrong');
+    });
 
     // Start the server
     app.listen(port, () => {
-        console.log(`App listening at http://localhost:${port}`);
+      console.log(`App listening at http://localhost:${port}`);
     });
-});
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
